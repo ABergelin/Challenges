ac7fb7dcdff8aedb0a43e6074d149c15da6a9d75
A Bergelin <the_bergelin@hotmail.co.uk>
2017-05-09 15:57:41 +0100

    Added link to site

--- Challenges/src/challenge1/sumForLoop.java
+++ Challenges/src/challenge1/sumForLoop.java
@@ -4,6 +4,8 @@ public class sumForLoop {
 
 	public static void main(String[] args) {
 		
+		//http://www.shiftedup.com/2015/05/07/five-programming-problems-every-software-engineer-should-be-able-to-solve-in-less-than-1-hour
+		
 		// Running a for loop to get the total of a list of numbers
 		int Numbers[] = {1,2,3,4,5,6,7,8,9,10};
 		int total = 0;
