9351ec34bd6773ef1f0c859465d42f09b13a56dd
A Bergelin <the_bergelin@hotmail.co.uk>
2017-05-09 14:09:01 +0100

    Committing for loop

--- Challenges/src/challenge1/sumForLoop.java
+++ Challenges/src/challenge1/sumForLoop.java
@@ -5,7 +5,16 @@ public class sumForLoop {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		//testing
+		int Numbers[] = {1,2,3,4,5,6,7,8,9,10};
+		int total = 0;
+		
+		for(int i = 0; i<Numbers.length;i++){
+			total = total + Numbers[i];
+		}
+		
+		
+		System.out.println("For loop Total = " + total);
+			
 
 	}
 
